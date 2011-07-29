4f24169d3dac9c389c4dc831e3dcc9edc19146bc
Tony Tam <fehguy@gmail.com>
2011-07-29 13:48:06 -0700

    added origin filter

---
+++ src/main/scala/com/wordnik/swagger/sample/util/ApiOriginFilter.scala
@@ -0,0 +1,22 @@
+package com.wordnik.swagr.sandbox.util
+
+import java.io.IOException
+
+import javax.servlet._
+import javax.servlet.http.HttpServletResponse
+
+class ApiOriginFilter extends javax.servlet.Filter {
+  @throws(classOf[IOException])
+  @throws(classOf[ServletException])
+  override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) = {
+    val res = response.asInstanceOf[HttpServletResponse]
+//  res.addHeader("Wordnik-API-Version", com.wordnik.env.Version.number());
+    res.addHeader("Access-Control-Allow-Origin:", "*");
+    chain.doFilter(request, response);
+  }
+
+  override def destroy() = {}
+
+  @throws(classOf[ServletException])
+  override def init(filterConfig: FilterConfig) {}
+}
