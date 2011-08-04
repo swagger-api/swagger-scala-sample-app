d8bdf8f36b2a248e9b8e1518cf92f6fcc666f27c
rpidikiti <ramesh@wordnik.com>
2011-08-04 07:33:11 -0700

    Added build for java library.

---
+++ sdk-libs/src/main/java/com/wordnik/swagger/sample/sdk/java/common/BaseObject.java
@@ -0,0 +1,11 @@
+package com.wordnik.swagger.sample.sdk.java.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ramesh
+ * Date: 8/2/11
+ * Time: 2:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseObject {
+}
