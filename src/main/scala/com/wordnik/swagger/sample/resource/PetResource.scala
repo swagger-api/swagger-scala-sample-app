e0eb67b85e11f3125e8df2f5a47603cfc18e1a47
rpidikiti <ramesh@wordnik.com>
2011-08-31 11:30:43 -0700

    Fixed typo with multi value response annotation

--- src/main/scala/com/wordnik/swagger/sample/resource/PetResource.scala
+++ src/main/scala/com/wordnik/swagger/sample/resource/PetResource.scala
@@ -77,7 +77,7 @@ trait PetResource extends RestResourceUtil {
   @Path("/findByStatus")
   @ApiOperation(value = "Finds Pets by status",
     notes = "Multiple status values can be provided with comma seperated strings",
-    responseClass = "com.wordnik.swagger.sample.model.Pet", mutiValueResponse = true)
+    responseClass = "com.wordnik.swagger.sample.model.Pet", multiValueResponse = true)
   @ApiErrors(Array(
     new ApiError(code = 400, reason = "Invalid status value")))
   def findPetsByStatus(
@@ -91,7 +91,7 @@ trait PetResource extends RestResourceUtil {
   @Path("/findByTags")
   @ApiOperation(value = "Finds Pets by tags",
     notes = "Muliple tags can be provided with comma seperated strings. Use tag1, tag2, tag3 for testing.",
-    responseClass = "com.wordnik.swagger.sample.model.Pet", mutiValueResponse = true)
+    responseClass = "com.wordnik.swagger.sample.model.Pet", multiValueResponse = true)
   @ApiErrors(Array(
     new ApiError(code = 400, reason = "Invalid tag value")))
   @Deprecated
